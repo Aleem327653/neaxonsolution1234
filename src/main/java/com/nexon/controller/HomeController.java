1ab8ab385e56ba1c7ccdc6f52315c3e8406db931
Yogesh <y314yadav@gmail.com>
2025-01-14 13:13:24 +0530

    feature2 solve bug

--- src/main/java/com/nexon/controller/HomeController.java
+++ src/main/java/com/nexon/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.nexon.controller;
 
 import com.nexon.model.Company;
+import com.nexon.service.CompanysServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/company")
 public class HomeController {
 
+    @Autowired
+    private CompanysServiceImp serviceImp;
     @PostMapping
     public Company addCompany(@RequestBody Company company){
-
+         return this.serviceImp.addCompany(company);
     }
 }
