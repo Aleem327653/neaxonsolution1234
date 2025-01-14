03f03a83d189d9c117b60d6585ebce5abb46b5bc
Yogesh <y314yadav@gmail.com>
2025-01-14 16:45:57 +0530

    update data via feature3

--- src/main/java/com/nexon/service/CompanysServiceImp.java
+++ src/main/java/com/nexon/service/CompanysServiceImp.java
@@ -25,4 +25,18 @@ public class CompanysServiceImp implements CompanyService{
     public List<Company> findAllCompany() {
         return repo.findAll();
     }
+
+    @Override
+    public Company findByCompanyName(String companyName) {
+        return repo.findByCompanyName(companyName);
+    }
+
+    @Override
+    public Company updateCompany(Company company, Integer id) {
+        Company com=repo.findById(id).get();
+        com.setCompanyName(company.getCompanyName());
+        com.setAddress(company.getAddress());
+        repo.save(com);
+        return com;
+    }
 }
