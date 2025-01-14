d25205ff2372bf15c55a657306ead96020bfbcf6
Yogesh <y314yadav@gmail.com>
2025-01-14 12:31:29 +0530

    Project created

---
+++ src/test/java/com/nexon/NexonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
