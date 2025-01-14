4d639f31212ee11e66f5f4743a030b1955b3ec9d
Yogesh <y314yadav@gmail.com>
2025-01-14 14:38:17 +0530

    solved Bug error

---
+++ src/main/java/com/nexon/controller/HomeController.java
@@ -0,0 +1,31 @@
+package com.nexon.controller;
+
+import com.nexon.model.Company;
+import com.nexon.service.CompanysServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/company")
+public class HomeController {
+
+    @Autowired
+    private CompanysServiceImp serviceImp;
+    @PostMapping
+    public Company addCompany(@RequestBody Company company)
+    {
+         return this.serviceImp.addCompany(company);
+    }
+
+    @GetMapping
+    public List<Company> getAllCompany(){
+        return this.serviceImp.findAllCompany();
+    }
+
+    @GetMapping("/{id}")
+    public Company singleCompnany(@PathVariable Integer id){
+        return this.serviceImp.fetchSingleCompany(id);
+    }
+}
